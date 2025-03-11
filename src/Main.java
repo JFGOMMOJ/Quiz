b6969ab39e0b4632b8a26001dec72754f8bdbdf2
JFGOMMOJ <jhonfredygomez@ucundinamarca.edu.co>
2025-03-10 21:40:33 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,133 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Producto> productos = new ArrayList<>(); // Lista para almacenar productos
+        Scanner teclado = new Scanner(System.in);
+        int a;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Bienvenidos a tu tienda de confianza");
+        do {
+            System.out.print("Que quieres hacer hoy?: " +
+                    "\n1. Crear producto:" +
+                    "\n2. Vender producto:" +
+                    "\n3. Reponer stock:" +
+                    "\n4. Aplicar descuento:" +
+                    "\n5. Mostrar información:" +
+                    "\n6. Aumentar precio:" +
+                    "\n7. Calcular valor total en inventario:" +
+                    "\n8. Eliminar productos por codigo."
+            );
+            a = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (a) {
+                case 1:
+                    System.out.println("Crear producto");
+                    Producto.crearProducto(teclado, productos);
+                    break;
+                case 2:
+                    System.out.println("Vender producto");
+                    //Revisa que halla stock del producto en el inventario
+                    if (productos.isEmpty()) {
+                        System.out.println("No hay productos en el inventario.");
+                    } else {
+                        System.out.print("Digite el Código del producto a vender: ");
+                        int codigoVender = teclado.nextInt();
+                        System.out.print("Cantidad a vender: ");
+                        int cantidadVender = teclado.nextInt();
+                        teclado.nextLine(); // Limpiar buffer
+                        // Busca la existencia del productp en el inventario
+                        boolean encontrado = false;
+                        for (Producto p : productos) {
+                            if (p.getCodigo_producto() == codigoVender) {
+                                p.vender_producto(cantidadVender);
+                                encontrado = true;
+                                break;
+                            }
+                        }
+
+                        if (!encontrado) {
+                            System.out.println("Producto no encontrado.");
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Reponer stock");
+                    System.out.print("Código del producto a reponer: ");
+                    int codigoReponer = teclado.nextInt();
+                    System.out.print("Cantidad a reponer: ");
+                    int cantidadReponer = teclado.nextInt();
+
+                    // Busca la existencia del productp en el inventario
+                    for (Producto p : productos) {
+                        if (p.getCodigo_producto() == codigoReponer) {
+                            p.reponer_stock(cantidadReponer);
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Aplicar descuento");
+                    System.out.print("Ingrese el código del producto para aplicar descuento: ");
+                    int codigoDescuento = teclado.nextInt();
+                    System.out.print("Ingrese el porcentaje de descuento a aplicar (sin el %): ");
+                    double porcentaje = teclado.nextDouble();
+                    // Busca la existencia del productp en el inventario
+                    boolean encontrado = false;
+                    for (Producto p : productos) {
+                        if (p.getCodigo_producto() == codigoDescuento) {
+                            p.aplicarDescuento(porcentaje);
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Producto no existe.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Mostrar información");
+                    System.out.print("Código del producto a mostrar: ");
+                    int codigoMostrar = teclado.nextInt();
+                    for (Producto p : productos) {
+                        if (p.getCodigo_producto() == codigoMostrar) {
+                            p.mostrarInformacion();
+                        }
+                    }
+                    break;
+                case 6:
+                    System.out.println("Aumentar precio");
+                    System.out.print("Digite el codigo del producto para aumentar precio: ");
+                    int codigoAumentar = teclado.nextInt();
+                    System.out.print("Ingrese el porcentaje de aumento (sin el %): ");
+                    double porcentajeAumento = teclado.nextDouble();
+
+                    boolean productoEncontrado = false;
+                    for (Producto p : productos) {
+                        if (p.getCodigo_producto() == codigoAumentar) {
+                            p.aumentarPrecio(porcentajeAumento);
+                            productoEncontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (!productoEncontrado) {
+                        System.out.println("⚠ Producto no encontrado.");
+                    }
+                    break;
+                case 7:
+                    Producto.calcularValorTotalInventario(productos);
+                    break;
+                case 8:
+                    Producto.eliminarProducto(productos, teclado);
+                    break;
+                default:
+                    System.out.println("No se aceptan opciones diferentes a las mencionadas anteriormente.");
+            }
+        } while (a != 0);
+
+        teclado.close(); // Cerrar el scanner
     }
 }
--- src/Producto.java
+++ src/Producto.java
@@ -1,4 +1,178 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Scanner;
 
 public class Producto {
+    private int codigo_producto;
+    private String nombre_producto;
+    private int precio_producto;
+    private int stock;
+
+    public Producto(int codigo_producto, String nombre_producto, int precio_producto, int stock) {
+        this.codigo_producto = codigo_producto;
+        this.nombre_producto = nombre_producto;
+        this.precio_producto = precio_producto;
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo_producto=" + codigo_producto +
+                ", nombre_producto=" + nombre_producto +
+                ", precio_producto=" + precio_producto +
+                ", stock=" + stock +
+                '}';
+    }
+
+    public int getCodigo_producto() {
+        return codigo_producto;
+    }
+
+    public void setCodigo_producto(int codigo_producto) {
+        this.codigo_producto = codigo_producto;
+    }
+
+    public int getPrecio_producto() {
+        return precio_producto;
+    }
+
+    public void setPrecio_producto(int precio_producto) {
+        this.precio_producto = precio_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // Este metodo es static, ya que no depende de una instancia específica de Producto.
+    public static void crearProducto(Scanner teclado, List<Producto> productos) {
+        System.out.print("Codigo Producto: ");
+        int codigo_producto = teclado.nextInt();
+        teclado.nextLine();
+
+        // Verificar si el código ya existe
+        for (Producto p : productos) {
+            if (p.getCodigo_producto() == codigo_producto) {
+                System.out.println("Error: Ya existe un producto con ese código.");
+                return;
+            }
+        }
+
+        System.out.print("Nombre Producto: ");
+        String nombre_producto = teclado.nextLine();
+        System.out.print("Precio  Producto: ");
+        int precio_producto = teclado.nextInt();
+        System.out.print("Cantidad en stock del producto: ");
+        int stock = teclado.nextInt();
+
+        Producto producto = new Producto(codigo_producto, nombre_producto, precio_producto, stock);
+        productos.add(producto);
+        System.out.println("Producto creado con exito.");
+    }
+
+    //Vender producto: Reduce la cantidad en stock si hay suficiente disponibilidad.
+    public void vender_producto(int cantidad) {
+        if (stock >= cantidad) {
+            stock -= cantidad;
+            System.out.println("Venta realizada con éxito. " +
+                    "\nEl valor a cobrar es: $"+ (cantidad * precio_producto) + " " +
+                    "\nQuedan " + stock + " unidades de " + nombre_producto + " en stock");
+        } else {
+            System.out.println("No hay suficiente stock disponible. Stock actual: " + stock);
+        }
+    }
+
+    // 3. Reponer stock: Aumenta la cantidad en stock cuando llega nueva mercancía.
+    public void reponer_stock(int cantidad) {
+        if (cantidad > 0) {
+            stock += cantidad;
+            System.out.println("Stock actualizado. " +
+                    "\nNueva cantidad: " + stock + " unidades de " + nombre_producto);
+        } else {
+            System.out.println("La cantidad a reponer debe ser mayor que cero");
+        }
+    }
+    // 4. Aplicar descuento: Reduce el precio del producto en un porcentaje dado(2%).
+    public void aplicarDescuento(double porcentaje) {
+        if (porcentaje > 0 && porcentaje <= 100) {
+            double descuento = precio_producto * (porcentaje / 100);
+            precio_producto -= descuento;
+            System.out.println("Se aplico un descuento del " + porcentaje + "%.");
+            System.out.println("Nuevo precio: " + precio_producto);
+        } else {
+            System.out.println("El porcentaje debe estar entre 1% y 100%.");
+        }
+    }
+
+    //5. Mostrar información: Imprime los detalles del producto. (NO toString)
+    public void mostrarInformacion() {
+        System.out.println("Código: " + codigo_producto);
+        System.out.println("Nombre: " + nombre_producto);
+        System.out.println("Precio: $" + precio_producto);
+        System.out.println("Stock disponible: " + stock);
+    }
+
+    //6. Aumentar precio: Incrementa el precio del producto en un porcentaje dado.(6%)
+    public void aumentarPrecio(double porcentaje) {
+        if (porcentaje > 0) {
+            double aumento = precio_producto * (porcentaje / 100);
+            precio_producto += aumento;
+            System.out.println("Se aumentó el precio en un " + porcentaje + "%.");
+            System.out.println("Nuevo precio: $" + precio_producto);
+        } else {
+            System.out.println("El porcentaje debe ser mayor a 0.");
+        }
+    }
+    //7. Calcular valor total en inventario:
+    public static void calcularValorTotalInventario(List<Producto> productos) {
+        double totalInventario = 0;
+
+        System.out.println("\n📦 Resumen del Inventario:");
+        if (productos.isEmpty()) {
+            System.out.println("⚠ No hay productos en el inventario.");
+        } else {
+            for (Producto p : productos) {
+                //Multiplica la cantidad en stock por el precio del producto.
+                double valorProducto = p.getPrecio_producto() * p.getStock();
+                System.out.println("Código: " + p.getCodigo_producto() +
+                        " | Nombre: " + p.getNombre_producto() +
+                        " | Precio: $" + p.getPrecio_producto() +
+                        " | Stock: " + p.getStock() +
+                        " | Valor Total: $" + valorProducto);
+                totalInventario += valorProducto;
+            }
+            //Mostrar total de inventario
+            System.out.println("\n💰 Total del inventario: $" + totalInventario);
+        }
+    }
+
+    //8. Eliminar productos por codigo.
+    public static void eliminarProducto(List<Producto> productos, Scanner teclado) {
+        System.out.println("\n Eliminar Producto");
+        System.out.print("Ingrese el código del producto: ");
+        int codigoEliminar = teclado.nextInt();
+
+        boolean eliminado = productos.removeIf(p -> p.getCodigo_producto() == codigoEliminar);
+        if (eliminado) {
+            System.out.println("Producto eliminado.");
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+    }
+
+
+
 }
+
